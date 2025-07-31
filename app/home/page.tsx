764aee11e8d04fcebde83f0d2a808d9a3a606b91
nicolas arsenault <nicolasarsenault@nicolass-MacBook-Pro.local>
2025-07-31 19:54:23 -0400

    fixed auth bug

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,13 @@
+'use client';
+
+import { createClient } from '@/utils/supabase/client';
 import React from 'react'
 
-function page() {
+async function page() {
+  const supabase = createClient();
+  const { data: { user } } = await supabase.auth.getUser()
+
+  
   return (
     <div>
       Welcome!
