c5b8207385279e9399a7bb856be7c1a75f65d92d
Nicolas Arsenault <temporaryjustice@outlook.com>
2025-10-01 10:26:30 -0400

    Delete SQL directory

--- SQL/MCQ-options.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Options (
-    option_id SERIAL PRIMARY KEY,
-    question_id INT NOT NULL,
-    option_text TEXT NOT NULL,
-    is_correct BOOLEAN DEFAULT FALSE,
-
-    FOREIGN KEY (question_id) REFERENCES Questions(question_id) ON DELETE CASCADE
-);
--- SQL/test.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Tests (
-    test_id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    is_timed BOOLEAN DEFAULT FALSE,
-    duration_minutes INT,  -- for timed tests
-    created_at TIMESTAMP DEFAULT NOW()
-);
